d7b3da2afbd78efc432dbed1bcc8139f199cf904
vfxtv-boop <bbaralangui@gmail.com>
2025-07-08 18:02:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 //auto presence update
-global.bankname = "MONIEPOINT"
+global.bankname = "xcursi"
 
-global.banknumber = "8133729715"
+global.banknumber = "242041029122"
 
 global.bankowner = "FADARE"
 
